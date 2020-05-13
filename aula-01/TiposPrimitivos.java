5de99a2280f63e54fd0d9f6391c7b9f4fb1b4435
Ezequiel Teixeira <ezequielteixeiracr@gmail.com>
2020-05-13 18:21:10 -0300

    Tipos Primitivos

---
+++ aula-01/TiposPrimitivos.java
@@ -0,0 +1,18 @@
+public class TiposPrimitivos {
+    
+    public static void main(String[] args) {
+        int numeroInteiro = 10;
+        byte valorEmByte = 10;
+        short valorInteiroPequeno = 1;
+        long valorInteiroGrande = 120;
+        float numeroFlutuante = 5.5;
+        double numeroFlutuante2 = 6.6;
+        char character = 'a';
+        boolean btrue = true;
+        boolean bfalse = false;
+
+        int a = 10; // a: 10
+        int b = a; // b: 10, a: 10
+        b = b + 1; // b: 11, a: 10
+    }
+}
