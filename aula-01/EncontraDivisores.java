9f2fad07898f9ec540455ac2675cbd60c606020d
Ezequiel Teixeira <ezequielteixeiracr@gmail.com>
2020-05-13 19:32:28 -0300

    Numeros impares

--- aula-01/EncontraDivisores.java
+++ aula-01/EncontraDivisores.java
@@ -3,15 +3,9 @@ public class EncontraDivisores {
     public static void main(String[] args) {
         int numero = 24;
 
-       int numeroDivisores = 0;
-        for(int i = numero; i >= 1; i--) {
-            if (numero % i == 0) {
-                System.out.println("Numero divisor " + i);
-
-                numeroDivisores++;
-                if (numeroDivisores == 3) {
-                    break;
-                }
+        for(int i = 1; i <= numero; i++) {
+            if (i % 2 == 1) {
+                System.out.println("Numero impares: " + i);
             }
         }
     }
