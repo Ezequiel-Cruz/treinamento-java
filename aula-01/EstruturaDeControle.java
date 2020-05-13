d1c1a9dfd1582633c09fe4df795b1d72af42cd01
Ezequiel Teixeira <ezequielteixeiracr@gmail.com>
2020-05-13 18:31:43 -0300

    Estrutura de controle

---
+++ aula-01/EstruturaDeControle.java
@@ -0,0 +1,12 @@
+public class EstruturaDeControle {
+    
+    public static void main(String[] args) {
+        int numero1 = 10;
+
+        if (numero1 % 2 == 0) {
+            System.out.println("O número informado é par!");
+        } else {
+            System.out.println("O número informado não é par!");
+        }
+    }
+}
