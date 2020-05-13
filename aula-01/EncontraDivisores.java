ff1c05fefcce95014130c8ed5028e5cd20733303
Ezequiel Teixeira <ezequielteixeiracr@gmail.com>
2020-05-13 19:25:38 -0300

    Ultimos divisores

--- aula-01/EncontraDivisores.java
+++ aula-01/EncontraDivisores.java
@@ -4,9 +4,9 @@ public class EncontraDivisores {
         int numero = 24;
 
        int numeroDivisores = 0;
-        for(int i = 1; i <= numero; i++) {
+        for(int i = numero; i >= 1; i--) {
             if (numero % i == 0) {
-                System.out.println("Numero divisor Crescente " + i);
+                System.out.println("Numero divisor " + i);
 
                 numeroDivisores++;
                 if (numeroDivisores == 3) {
