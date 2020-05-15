ea095a4e11bbdd75fbe07827e0a5b86c60861906
Ezequiel Teixeira <ezequielteixeiracr@gmail.com>
2020-05-14 21:18:21 -0300

    Ajustes finais

--- aula-02/MotorBanco/Motor.java
+++ aula-02/MotorBanco/Motor.java
@@ -1,22 +1,21 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap; 
 
 class Usuario{ 
     private String nome;
-    private Conta conta;
 
     Usuario(String nome) {
         this.nome = nome;
     }
 
-    public void criarConta() {
-        this.conta = new Conta();
-    }
-
-    public Conta obterConta() {
-        return this.conta;
+    public String obterNome() {
+        return this.nome;
     }
 }
 
-class Conta{
+class Conta {
     private Double valor;
 
     public void depositar(Double valor) { 
@@ -42,6 +41,45 @@ class Conta{
 
 }
 
+class Banco {
+    private Map<Usuario, Conta> mapaUsuarioConta;
+
+    Banco(){
+        this.mapaUsuarioConta = new HashMap();
+    }
+
+    public void criarConta(Usuario usuario) {
+        Conta novaConta = new Conta();
+
+        this.mapaUsuarioConta.put(usuario, novaConta);
+    }
+
+    public void verTodosOsSaldos() {
+        for(Usuario u: this.mapaUsuarioConta.keySet()) {
+            System.out.println(u.obterNome() + ":");
+            this.mapaUsuarioConta.get(u).verSaldo();
+            System.out.println("------------------------");
+        }
+    }
+
+    public void depositar(Double valor, Usuario usuario) { 
+        Conta conta = this.mapaUsuarioConta.get(usuario);
+        conta.depositar(valor);
+    }
+    
+    public Double sacar(Double valor, Usuario usuario) {
+        Conta conta = this.mapaUsuarioConta.get(usuario);
+        conta.sacar(valor);
+        return valor;
+    }
+
+    public void transferir(Double valor, Usuario origem, Usuario destino) {
+        Conta contaOrigem = this.mapaUsuarioConta.get(origem);
+        Conta contaDestino = this.mapaUsuarioConta.get(destino);
+        contaOrigem.transferir(valor, contaDestino);
+    }
+}
+
 public class Motor {
 
     public static void main(String[] args) {
@@ -50,20 +88,15 @@ public class Motor {
         //Maria
         Usuario maria = new Usuario("maria");
 
-        // Operação
-        joao.criarConta();
-        maria.criarConta();
+        Banco banco = new Banco();
 
-        joao.obterConta().depositar(50.0);
+        banco.criarConta(joao);
+        banco.criarConta(maria);
+    
+        banco.depositar(50.0, joao);
+        banco.sacar(10.0, maria);
+        banco.transferir(10.0, joao, maria);
 
-        maria.obterConta().sacar(10.0);
-
-        joao.obterConta().transferir(10.0, maria.obterConta());
-
-        System.out.println("João");
-        joao.obterConta().verSaldo();
-
-        System.out.println("Maria");
-        maria.obterConta().verSaldo();
+        banco.verTodosOsSaldos();
     }
 }
