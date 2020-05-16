89059c3d6f404a70eaa3c849061e28d54cd0237a
Ezequiel Teixeira Cruz <ecruz.radix@ons.org.br>
2020-05-16 14:29:17 -0300

    Aula 2

---
+++ alura/oop/src/CriarConta.java
@@ -0,0 +1,18 @@
+
+public class CriarConta {
+    public static void main(String[] args) {
+        Conta primeiraConta = new Conta();
+        Conta segundaConta = new Conta();
+
+        primeiraConta.saldo = 200;
+
+        System.out.println("Primeira Conta: " + primeiraConta.saldo);
+
+        primeiraConta.saldo += 120;
+        System.out.println("Primeira Conta Saldo Atualizado: " + primeiraConta.saldo);
+
+        segundaConta.saldo = 550;
+        System.out.println("Segunda conta: " + segundaConta.saldo);
+
+    }
+}
